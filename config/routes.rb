45ec1c697130bdd43ea8b74c90676978468ad2e8
Balint Handler <handlerbalint@gmail.com>
2021-05-26 09:28:05 +0200

    edit, update, show, index methods added to controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
-  resources :users, only: [:create, :new]
+  root 'users#index'
+  
+  resources :users, only: [:create, :new, :edit, :index, :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
